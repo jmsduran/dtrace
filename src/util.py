684122ad0b484c954db7b55e21a8586ddec86d42
James Duran <jmsduran@gmail.com>
2020-08-01 22:15:35 +0000

    Added copyright message to util.

--- src/util.py
+++ src/util.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 def equals(a, b, **kwargs):
     epsilon = kwargs.get('epsilon', .00001)
     diff = abs(a - b)
